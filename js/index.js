60ef84aebac2ef81c275009a2f950dae24b7372e
Sergiu-D <61351750+Sergiu-D@users.noreply.github.com>
2023-01-24 13:37:16 +0200

    installed lozad pkg

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-window.onload = function() {
+window.onload = function () {
   document.querySelector(".loading-screen").style.display = "none";
 };
+
+const observer = lozad(".lozad", {
+  rootMargin: "10px 0px", // syntax similar to that of CSS Margin
+  threshold: 0.1, // ratio of element convergence
+  loaded: function (el) {
+    // Custom implementation on a loaded element
+    el.classList.add("loaded");
+  },
+});
+observer.observe();
